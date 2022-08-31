88d22872dc2c07ff47225a91b190aa550a2bb109
Alexander <saniareva@gmail.com>
2022-08-31 12:17:57 +0300

    add

---
+++ Movies List/TableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  TableViewCell.swift
+//  Movies List
+//
+//  Created by Alex on 31.08.2022.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+    @IBOutlet weak var stringLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
--- Movies List/ViewController.swift
+++ Movies List/ViewController.swift
@@ -8,12 +8,51 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var titleTF: UITextField!
+    @IBOutlet weak var yearTF: UITextField!
+    @IBOutlet weak var tableView: UITableView!
+    
+    var titleList: [String] = []
+    var yearList: [String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        createTable()
     }
-
-
+    
+    func createTable() {
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+    
+    @IBAction func addButton(_ sender: UIButton) {
+        print(1)
+        if titleList.contains(titleTF.text ?? "") {
+            titleTF.text = "this movie is added"
+            yearTF.text = ""
+            print(2)
+        } else {
+            titleList.append(titleTF.text ?? "")
+            yearList.append(yearTF.text ?? "")
+            tableView.reloadData()
+            titleTF.text = ""
+            yearTF.text = ""
+            print(3)
+        }
+    }
 }
 
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TableViewCell",
+                                                 for: indexPath) as! TableViewCell
+        cell.stringLabel.text = "\(titleList[indexPath.row]) \(yearList[indexPath.row])"
+        return cell
+    }
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return titleList.count
+    }
+}
